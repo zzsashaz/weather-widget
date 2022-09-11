2fdbffd50305695d5d0b8b69c4d001ba56207690
Sasha <zzsashaz@gmail.com>
2022-09-11 15:07:08 +0300

    feat: add refresh cooldown

--- src/components/app/WidgetHeader.vue
+++ src/components/app/WidgetHeader.vue
@@ -1,18 +1,38 @@
 <template>
 <div class="header">
-  <icon icon-type="refresh" clickable @click="refreshData"/>
+  <div class="header__refresh">
+    <icon v-if="!coolDownSeconds" icon-type="refresh" clickable @click="refreshData"/>
+    <span v-if="coolDownSeconds">{{$t('measures.seconds',{ value:this.coolDownSeconds })}}</span>
+  </div>
   <icon icon-type="gear" clickable/>
 </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
   name: 'widget-header',
+  data() {
+    return {
+      coolDownSeconds: 0,
+      coolDownIntervalId: 0,
+    };
+  },
   methods: {
     async refreshData() {
       await this.$store.dispatch('fetchCurrentLocationWeatherData');
+      this.initRefreshCoolDown(30);
+    },
+    initRefreshCoolDown(seconds:number) {
+      this.coolDownSeconds = seconds;
+      this.coolDownIntervalId = setInterval(() => {
+        console.log(this.coolDownSeconds);
+        this.coolDownSeconds -= 1;
+        if (this.coolDownSeconds === 0) {
+          clearInterval(this.coolDownIntervalId);
+        }
+      }, 1000);
     },
   },
 });
